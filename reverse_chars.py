04c43bccb51a6a350da30b3562dc48d3195ddc6d
0xCoto <cotothearcher@gmail.com>
2019-02-20 21:49:49 +0200

    Update reverse_chars.py

--- reverse_chars.py
+++ reverse_chars.py
@@ -1,54 +1,60 @@
-##DEFINE FUNCTION##
-#Define binary search
-def bis(alpha, target):
-    lower = 0
-    upper = len(alpha)
-    while lower < upper:
-        x = lower + (upper - lower) // 2
-        val = alpha[x]
-        if(target == val):
-            return x
-        elif (target > val):
-            if(lower == x):
-                break #Break out of condition
-            lower = x
-        elif(target < val):
-            upper = x
+#Import library
+import string
 
+#Open file 'abc.txt'
+q = open("abc.txt", "r")
 
-#Define list of defaults elements
-alpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+### DEFINE LIST
+llist=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 statistics=[0]
-#Begin loop
+
 for i in range(25):
     statistics.append(0)
-print(len(alpha),len(statistics))
-lines=input()
-doesnotmatter=len(lines)
-for i in range(doesnotmatter):
-    target=bis(alpha, lines[i])
-    statistics[target]=statistics[target]+1
 
-#Get Max and Min
-#CHECK: each letter, appears at least once in the txt file
-for i in range(25):
-    if(statistics[i]>statistics[i+1]):
-        maxl=alpha[i]
-    if(statistics[i]<statistics[i+1]):
-        if((statistics[i]!=0) and (statistics[i+1]!=0)):
-            minl=alpha[i]
+w = []
 
-#Reverse loop
-for i in range(doesnotmatter):
-    if(maxl==lines[i]):
-        lines[i]==minl
-    if(minl==lines[i]):
-        lines[i]==maxl
+with open("abc.txt", "r") as q:
+    for i in q.read():
+        w.append(i)
 
-#Output lines
-print(lines)
+#convert Upper chars into lower
+w = [element.lower() for element in w];
+#Take number of appers for each alphabetic char
+### RUN LOOP
+for i in range(len(w)):
+    for j in range(len(llist)):
+        if(w[i]==llist[j]):
+            statistics[j]=statistics[j]+1
+### END LOOP
 
-#Repeat 25 times
-for i in range(25):
-    print(alpha[i], statistics[i])
+#Reverse time
+i=0
+while(i<=len(statistics)):
+    if(statistics!=0):
+        max=0
+        min=i
+        break
+    else:
+        i=i+1
+
+#Spot the characters
+for i in range(len(statistics)):
+    if((statistics[i]>statistics[max])):
+        max=i
+    if((statistics[i]<statistics[min])and(statistics[i]!=0)):
+        min=i
+
+#Revserse chars
+for i in range(len(p)):
+    if(w[i]==llist[max]):
+        w[i]=llist[min]
+    elif(w[i]==llist[min]):
+        w[i]=llist[max]
+        
+### List-to-string conversion
+
+w = ''.join(w)
+
+#Print the final output
+print(w.upper())
